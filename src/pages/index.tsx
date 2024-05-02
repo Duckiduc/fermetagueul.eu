20167bfacb4fa648dd43c5110f9ae1c040062473
NGUYEN Duc-Thomas 00071703 <duc-thomas.nguyen@3ds.com>
2024-05-02 12:22:40 +0200

    Update Home page styling in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-2 lg:p-24 ${inter.className}`}
     >
       <header>
         <div className="flex items-end mb-24">
